221b7e5facb24ee2f92181d43fe6f5f2b05f64a5
fernandpaiva <fernandopaiva1705@gmail.com>
2024-03-11 22:16:19 -0300

    Adicionar arquivos de projeto.

---
+++ gift list/gift list/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace gift_list.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
